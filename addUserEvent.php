54d8492721bc40087b64d8ec664d3694b4c19df7
JimCloudy <lynchbrent@hotmail.com>
2013-01-18 12:16:57 -0600

    moving recent changes

--- addUserEvent.php
+++ addUserEvent.php
@@ -6,7 +6,8 @@
 	$mysql_password = "6doubles";
 	$con = mysql_connect($mysql_host,$mysql_user,$mysql_password);
 	mysql_select_db($mysql_database,$con);
-	$query = "INSERT INTO events (Event_Name, Event_Date, Event_Time, Event_Location, Event_City, Event_State, Event_Website) VALUES('$_POST[event]','$_POST[date]','$_POST[time]','$_POST[location]','$_POST[city]','$_POST[state]','$_POST[website]')";
+	$eventDate = date('Y-m-d',strtotime($_POST[date]));
+	$query = "INSERT INTO events (U_Id,Event_Name, Event_Date, Event_Time, Event_Location, Event_City, Event_State, Event_Website) VALUES('$_SESSION[userid]','$_POST[event]','$eventDate','$_POST[time]','$_POST[location]','$_POST[city]','$_POST[state]','$_POST[website]')";
 	if(!mysql_query($query,$con)){
 		echo $query . "<br>";
 		echo mysql_error();
